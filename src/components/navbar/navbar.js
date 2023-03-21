2b83776d03a3fac012fb94a3b2ee07549ba3107c
FranciscoGontijo <franciscoacmg@gmail.com>
2023-03-21 21:40:20 +1100

    Nav Bar structure

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,35 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 import './navbar.css';
 
 const NavigationBar = () => {
+
     return (
-        <nav className="sidebar-nav">
-            <div className="logo-container">
-                <img src='#' alt="logo" className="logo" />
-            </div>
-            <ul>
-                <li className="nav-link">
-                    <Link className="link" to="/">Home</Link>
-                </li>
-                <li className="nav-link">
-                    <Link className="link" to="/about">About</Link>
-                </li>
-                <li className="nav-link">
-                    <Link className="link" to="/models">Vehicle Models</Link>
-                </li>
-                <li className="nav-link">
-                    <Link className="link" to="/testimonials">Testimonials</Link>
-                </li>
-                <li className="nav-link">
-                    <Link className="link" to="/team">Our Team</Link>
-                </li>
-                <li className="nav-link">
-                    <Link className="link" to="/contact">Contact</Link>
-                </li>
-            </ul>
-        </nav>
+        <>
+            <nav className="nav-bar">
+                <div className="logo-container">
+                    <img src='#' alt="logo" className="logo" />
+                </div>
+                <button className="nav-bar-button">Nav icon</button>
+                <ul className="nav-bar-link-list">
+                    <li className="nav-link">
+                        <Link className="link" to="/">Home</Link>
+                    </li>
+                    <li className="nav-link">
+                        <Link className="link" to="/about">About</Link>
+                    </li>
+                    <li className="nav-link">
+                        <Link className="link" to="/models">Vehicle Models</Link>
+                    </li>
+                    <li className="nav-link">
+                        <Link className="link" to="/testimonials">Testimonials</Link>
+                    </li>
+                    <li className="nav-link">
+                        <Link className="link" to="/team">Our Team</Link>
+                    </li>
+                    <li className="nav-link">
+                        <Link className="link" to="/contact">Contact</Link>
+                    </li>
+                </ul>
+            </nav>
+        </>
     )
 };
 
