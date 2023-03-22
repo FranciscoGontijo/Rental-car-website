05f2bf2f846dff87c0809cc668c20c8303a5e310
FranciscoGontijo <franciscoacmg@gmail.com>
2023-03-22 22:33:27 +1100

    Why choose us CSS

--- src/components/home/whyusbanner.js
+++ src/components/home/whyusbanner.js
@@ -2,11 +2,9 @@ import React from "react";
 
 const WhyUsBanner = () => {
     return (
-        <section>
-            <div>
-                <img />
-            </div>
-            <div>
+        <section className="why-choose-us-container">
+            <img src="#" />
+            <div className="why-choose-us-banner">
                 <h3>Why Choose Us</h3>
                 <h2>Best valued deals you will ever find</h2>
                 <p>Discover the best deals you'll ever find with our unbeatable offers.
@@ -15,22 +13,25 @@ const WhyUsBanner = () => {
                     Our deals are designed to give you the ultimate renting experience,
                     so don't miss out on your chance to save big.</p>
             </div>
-            <button>Find Details</button>
-            <div>
-                <img src="#" alt="car image" />
-                <h3>Cross country drive</h3>
-                <p>Take your driving experience to the next level with our top-notch vehicles for your cross-country adventures.</p>
+            <button className="why-choose-us-button">Find Details</button>
+            <div className="why-choose-us-boxes-container">
+                <div className="why-choose-us-boxes">
+                    <img src="#" alt="car image" />
+                    <h3>Cross country drive</h3>
+                    <p>Take your driving experience to the next level with our top-notch vehicles for your cross-country adventures.</p>
+                </div>
+                <div className="why-choose-us-boxes">
+                    <img src="#" alt="money image" />
+                    <h3>All Inclusive pricing</h3>
+                    <p>Get everything you need in one convenient, transparent price with our all-inclusive pricing policy.</p>
+                </div>
+                <div className="why-choose-us-boxes">
+                    <img src="#" alt="hands holding a coin image" />
+                    <h3>No hidden charges</h3>
+                    <p>Enjoy peace of mind with our no hidden charges policy. We believe in transparent and honest pricing.</p>
+                </div>
             </div>
-            <div>
-                <img src="#" alt="money image" />
-                <h3>All Inclusive pricing</h3>
-                <p>Get everything you need in one convenient, transparent price with our all-inclusive pricing policy.</p>
-            </div>
-            <div>
-                <img src="#" alt="hands holding a coin image" />
-                <h3>No hidden charges</h3>
-                <p>Enjoy peace of mind with our no hidden charges policy. We believe in transparent and honest pricing.</p>
-            </div>
+
         </section>
     )
 };
