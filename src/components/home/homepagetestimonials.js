d561862139e0b68befd85ee66184adcf9499f4ce
FranciscoGontijo <franciscoacmg@gmail.com>
2023-03-22 23:14:07 +1100

    Home page testimonials CSS

--- src/components/home/homepagetestimonials.js
+++ src/components/home/homepagetestimonials.js
@@ -5,20 +5,20 @@ import './bookacar.css';
 const HomePageTestimonials = () => {
     return (
         <section className="home-page-testimonials-container">
-            <div>
+            <div className="home-page-testemonials-banner">
                 <h3>Reviewed by People</h3>
                 <h2>Client's Testimonials</h2>
                 <p>Discover the positive impact we've made on the our clients by reading through their testimonials.
                     Our clients have experienced our service and results,
                     and they're eager to share their positive experiences with you.</p>
             </div>
-            <div>
+            <div className="testimonials-boxes">
                 <p>"We rented a car from this website and had an amazing experience! The booking was easy and the rental rates were very affordable. "</p>
-                <div>
+                <div className="testimonials-boxes-profile-container">
                     <img src="#" alt="Profile photo" />
-                    <div>
-                        <h3>Person Name</h3>
-                        <h5>City</h5>
+                    <div className="testimonials-boxes-name-container">
+                        <h4>Person Name</h4>
+                        <p>City</p>
                     </div>
                 </div>
             </div>
