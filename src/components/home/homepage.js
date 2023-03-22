76a3c674d80dd8b1158f1cc5d2c7097594d87691
FranciscoGontijo <franciscoacmg@gmail.com>
2023-03-22 12:45:45 +1100

    Hero banner CSS

--- src/components/home/homepage.js
+++ src/components/home/homepage.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+//Import Components
 import BookACar from './bookacar';
 import RentalFleet from './vehicleshomepage';
 import WhyUsBanner from './whyusbanner';
@@ -8,16 +9,23 @@ import HomePageFAQ from './homepageFAQ';
 import DownloadAppSection from './downloadappsection';
 import Footer from '../footer/footer';
 
+//Import stylesheet
+import "./homepage.css";
+
 const HomePage = () => {
     return (
         <section className='home-page-container'>
+            <img className="background-hero-image" src="#"/>
+            <img className='background-hero-car-image' src="#"/>
             <div className='home-page-hero-banner'>
-                <h3>Plan your trip now</h3>
-                <h1>Save <span>big</span> with our car rental</h1>
-                <p>Rent the car of your dreams. Unbeatable prices, unlimited miles, flexible pick-up options and much more.</p>
-                <div>
-                    <button>Book Ride</button>
-                    <button>Learn More</button>
+                <div className='home-page-hero-banner-text-container'>
+                    <h4>Plan your trip now</h4>
+                    <h1>Save <span>big</span> with our car rental</h1>
+                    <p>Rent the car of your dreams. Unbeatable prices, unlimited miles, flexible pick-up options and much more.</p>
+                </div>
+                <div className='home-page-hero-banner-buttons-container'>
+                    <button className='orange-button'>Book Ride<i></i></button>
+                    <button className='black-button'>Learn More<i></i></button>
                 </div>
             </div>
             <BookACar />
@@ -46,10 +54,10 @@ const HomePage = () => {
                 <p>Top Airports. Local suppliers. 24/7 Support.</p>
             </div>
             <WhyUsBanner />
-            <HomePageTestimonials/>
+            <HomePageTestimonials />
             <HomePageFAQ />
-            <DownloadAppSection/>
-            <Footer/>
+            <DownloadAppSection />
+            <Footer />
         </section>
     )
 };
