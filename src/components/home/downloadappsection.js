cd26a09b2202b6e7a9b3bec99aa55fdcc8028245
FranciscoGontijo <franciscoacmg@gmail.com>
2023-03-23 18:51:19 +1100

    Download app section structure and CSS

--- src/components/home/downloadappsection.js
+++ src/components/home/downloadappsection.js
@@ -1,9 +1,18 @@
 import React from "react";
 
+import "./homepage.css"
+
 const DownloadAppSection = () => {
     return (
-        <section>
-            <h1>Download App Section</h1>
+        <section className="home-page-download-app-container">
+            <h2>Download our app to get most out of it</h2>
+            <p>Thrown shy denote ten ladies though ask saw. 
+                Or by to he going think order event music. 
+                Incommode so intention defective at convinced. 
+                Led income months itself and houses you.
+            </p>
+            <img src="#" alt="Download from goodle play" />
+            <img src="#" alt="Download from apple store" />
         </section>
     )
 };
