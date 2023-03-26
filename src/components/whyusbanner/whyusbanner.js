339381573611fa802d83d6e5a263980b8435b1b6
FranciscoGontijo <franciscoacmg@gmail.com>
2023-03-27 09:38:24 +1100

    Add images to why choose us banner

--- src/components/whyusbanner/whyusbanner.js
+++ src/components/whyusbanner/whyusbanner.js
@@ -2,10 +2,16 @@ import React from "react";
 
 import "./whyusbanner.css";
 
+//import image
+import whyChooseUseBannerSrc from "../../assets/images/why-choose-us-main-image.png";
+import crossCountryLogoSrc from "../../assets/images/cross-country-drive-logo.png";
+import allInclusiveLogoSrc from "../../assets/images/all-inclusive-pricing-logo.png";
+import hiddenChargesLogoSrc from "../../assets/images/no-hidden-charges-logo.png";
+
 const WhyUsBanner = () => {
     return (
         <section className="why-choose-us-container">
-            <img src="#" />
+            <img src={whyChooseUseBannerSrc} className="why-choose-us-main-img" />
             <div className="why-choose-us-banner">
                 <h3>Why Choose Us</h3>
                 <h2>Best valued deals you will ever find</h2>
@@ -14,21 +20,21 @@ const WhyUsBanner = () => {
                     so you can enjoy top-quality services and products without breaking the bank.
                     Our deals are designed to give you the ultimate renting experience,
                     so don't miss out on your chance to save big.</p>
+                    <button className="why-choose-us-button">Find Details</button>
             </div>
-            <button className="why-choose-us-button">Find Details</button>
             <div className="why-choose-us-boxes-container">
                 <div className="why-choose-us-boxes">
-                    <img src="#" alt="car image" />
+                    <img src={crossCountryLogoSrc} alt="car image" />
                     <h3>Cross country drive</h3>
                     <p>Take your driving experience to the next level with our top-notch vehicles for your cross-country adventures.</p>
                 </div>
                 <div className="why-choose-us-boxes">
-                    <img src="#" alt="money image" />
+                    <img src={allInclusiveLogoSrc} alt="money image" />
                     <h3>All Inclusive pricing</h3>
                     <p>Get everything you need in one convenient, transparent price with our all-inclusive pricing policy.</p>
                 </div>
                 <div className="why-choose-us-boxes">
-                    <img src="#" alt="hands holding a coin image" />
+                    <img src={hiddenChargesLogoSrc} alt="hands holding a coin image" />
                     <h3>No hidden charges</h3>
                     <p>Enjoy peace of mind with our no hidden charges policy. We believe in transparent and honest pricing.</p>
                 </div>
