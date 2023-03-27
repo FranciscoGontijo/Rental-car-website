919e552f16ff1f472dc03bceca79051d5e233d76
FranciscoGontijo <franciscoacmg@gmail.com>
2023-03-27 15:40:50 +1100

    Hidden nav bar structure and CSS, with full functionality. Nav-bar logo image

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -3,15 +3,40 @@ import { Link } from "react-router-dom";
 
 import './navbar.css';
 
+//import images
+import navBarLogoSrc from "../../assets/images/nav-bar-logo.png";
+
 const NavigationBar = () => {
+    const [navigationDisplay, setNavigationDisplay] = useState(false);
 
     return (
         <>
             <nav className="nav-bar">
                 <div className="logo-container">
-                    <img src='#' alt="logo" className="logo" />
+                    <img src={navBarLogoSrc} alt="logo" className="logo" />
                 </div>
-                <button className="nav-bar-button">Nav icon</button>
+                <button onClick={() => setNavigationDisplay(true)} className="nav-bar-button"><i class="fa-solid fa-bars"></i></button>
+                {navigationDisplay && <i onClick={() => setNavigationDisplay(false)} className="fa-solid fa-xmark close-nav-bar"></i>}
+                {navigationDisplay && <ul className="hidden-nav-bar">
+                    <li className="nav-link" onClick={() => setNavigationDisplay(false)}>
+                        <Link className="hidden-link" to="/">Home</Link>
+                    </li>
+                    <li className="nav-link" onClick={() => setNavigationDisplay(false)}>
+                        <Link className="hidden-link" to="/about">About</Link>
+                    </li>
+                    <li className="nav-link" onClick={() => setNavigationDisplay(false)}>
+                        <Link className="hidden-link" to="/models">Vehicle Models</Link>
+                    </li>
+                    <li className="nav-link" onClick={() => setNavigationDisplay(false)}>
+                        <Link className="hidden-link" to="/testimonials">Testimonials</Link>
+                    </li>
+                    <li className="nav-link" onClick={() => setNavigationDisplay(false)}>
+                        <Link className="hidden-link" to="/team">Our Team</Link>
+                    </li>
+                    <li className="nav-link" onClick={() => setNavigationDisplay(false)}>
+                        <Link className="hidden-link" to="/contact">Contact</Link>
+                    </li>
+                </ul>}
                 <ul className="nav-bar-link-list">
                     <li className="nav-link">
                         <Link className="link" to="/">Home</Link>
